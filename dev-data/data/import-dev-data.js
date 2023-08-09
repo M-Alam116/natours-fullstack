1e5e0647a38482a46ec9c6c0cf14affe604fce64
Muhammad Alam <malam53345@gmail.com>
2023-08-10 00:12:55 +0500

    first commit

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const Tour = require('./../../models/tourModel');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successfull');
+  });
+
+// Read json file
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+// Import data into database
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data sucessfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// Delete data from database
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data sucessfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
